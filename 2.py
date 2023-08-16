e4b5fb3ca76d5c4c1debed9ca14451d8190a36ef
charan <charan@gmail.com>
2023-08-16 14:17:48 +0530

    change1

---
+++ 2.py
@@ -0,0 +1,6 @@
+import numpy as np
+arr=np.array([[1,2,3],[1,2,3]])
+print("dimension=",arr.ndim)
+print("shape of array",arr.shape)
+print("data type of array",arr.dtype)
+print("size of array",arr.size)
